0edf3b000f1b8bfa8837f7cd063da0a4bd48767e
Diego Maldonado <damaldonadoc@protonmail.com>
2017-02-21 09:07:18 -0300

    fix paste indentation bug in web-mode

--- core/core-web.el
+++ core/core-web.el
@@ -5,6 +5,7 @@
         web-mode-code-indent-offset 2
 	web-mode-enable-current-element-highlight t)
   (setq web-mode-enable-auto-pairing nil)
+  (add-hook 'web-mode-hook (lambda () (electric-indent-local-mode -1)))
 
   (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
